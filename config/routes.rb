b23779e605eac45b612adb05fc3a9177e372c257
belliis <belljjs@gmail.com>
2019-04-15 23:29:14 -0700

    Routing and authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
-  resources :joints
-  resources :outfits
-  resources :weathers
-  resources :items
-  resources :users
-  resources :occasions
-  resources :colors
-  resources :categories
-  resources :seasons
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :items do
+    get :delete, on: :collection
+    get :get_sample, on: :collection
+    resources :joints, shallow: true, only: [:create, :destroy]
+  end
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :destroy]
+
+  resources :outfits, only: [:new, :create, :index, :destroy] do
+    get :start, on: :collection
+  end
+
+  get('/', { to: 'outfits#start', as: 'first' })
+  
+
 end
